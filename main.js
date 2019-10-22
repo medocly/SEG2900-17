920b4fdd9f3e0927ffdb44baf946044701363b01
Sam Sunny <sampsunny1995@gmail.com>
2019-10-21 20:36:01 -0400

    added a website loading bar

---
+++ main.js
@@ -0,0 +1,8 @@
+// Loader
+window.addEventListener("load", function(){
+    var loader = document.getElementById("loader");
+    window.setTimeout(function(){
+    document.body.removeChild(loader)
+    },300);
+    
+  });
